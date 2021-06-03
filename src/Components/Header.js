3bea6833ca9af613c67b02a8b31b2f9ff9e02ead
adarshtiwari1998 <tiwari1998adarsh@gmail.com>
2021-06-03 19:39:48 +0530

    pages-> home pages added and components-> Header pages added

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "../style/header.module.css";
+import {Link} from "react-router-dom";
+
+function Header() {
+    return (
+        <div>
+            Header
+        </div>
+    )
+}
+
+export default Header;
