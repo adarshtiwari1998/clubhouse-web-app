deb7f8013b49beb8f2fd9ed127b5f3328b4bec62
adarshtiwari1998 <tiwari1998adarsh@gmail.com>
2021-06-29 12:58:42 +0530

    bottom sheet-> start room components created

--- src/Components/BottomSheet.js
+++ src/Components/BottomSheet.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SwipeableBottomSheet from "react-swipeable-bottom-sheet";
-import style from  "../style/bottomSheet.module.css"
+import style from  "../style/bottomSheet.module.css";
+import StartRoom from "./bottom_sheet/StartRoom";
+
 
 function BottomSheet(props) {
     return (
-       <SwipeableBottomSheet 
+       <SwipeableBottomSheet style={{width: "34%", margin: "auto"}}
        open={props.sheetVisible}
         onChange={() => {
          props.setSheetVisible(!props.sheetVisible);
@@ -13,6 +15,12 @@ function BottomSheet(props) {
         fullScreen={props.sheetTitle === "room detail" ? true : false} >
         <div className={style.BottomSheetContainer} 
          style={{backgroundColor: props.sheetTitle === "profile" ? "transparent" : ""}}>
+        <StartRoom setSheetCreateRoom={props.setSheetCreateRoom} set
+           setSheetVisible={(item) => {
+           props.setSheetVisible(item);
+           props.setItemsVisible(true);
+           }}
+        />
         </div>
        </SwipeableBottomSheet>
     );
---
+++ src/Components/bottom_sheet/StartRoom.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import style from "../../style/bottomSheet.module.css";
+import {FcGlobe} from "react-icons/fc";
+
+
+function StartRoom(props) {
+    const [room, setRoom] = useState("open")
+    return (
+        <>
+        <div className={style.switch_Line}></div>
+          <div className="text-right">
+              <button className={style.addTopicBtn}>
+               Add a Topic
+              </button>
+          </div>
+          <div className={style.selectRoom}>
+              <button className={room == "open" ? style.active : "" }
+              onClick={() => setRoom("open")}
+              >
+              <div>
+                  <FcGlobe />
+              </div>
+              Open
+              </button>
+              <button className={room == "social" ? style.active : "" }
+              onClick={() => setRoom("social")}
+              >
+              <div>
+              <FcGlobe />
+              </div>
+              Social
+              </button>
+              <button className={room == "closed" ? style.active : "" }
+              onClick={() => setRoom("closed")}
+              >
+              <div>
+              <FcGlobe />
+              </div>
+              Closed
+              </button>
+          </div>  
+          <p>Start a Room {" "}
+         {/* multiple nested if else statement       */}
+        <span>{room == "closed" ? "for people i choose" : room == "social" ? "with people i follow" : "open to everyone"}</span></p>  
+        <div className="text-center">
+         <button className={style.letGoBtn}  
+          onClick={() => {
+            props.setsheetCreateRoom(true);
+            props.setSheetVisible(true);
+          }}>🚀Let's Go 
+        </button>    
+        </div> 
+        </>
+    )
+}
+
+export default StartRoom;
