d1bd3e9542680913b2ba702d48e3f69a71c1a08a
adarshtiwari1998 <tiwari1998adarsh@gmail.com>
2021-08-05 18:58:02 +0530

    pages-

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import style from "../style/explore.module.css";
 import { DownOutlined, FireOutlined } from "@ant-design/icons";
+import {Input} from "antd";
 import data from "../Data/exploreData.json"
 
 function Explore() {
     const { people, conversation } = data;
     return (
         <div className={style.exploreContainer}>
+            {/* mainheader */}
+            <div className={style.exploreheader}>
+              {/* subheader */}
+              <Input style={{
+               backgroundColor: "#f4f4f4",
+               borderRadius: "0.9em",
+               outline: "none",
+               padding: "0.3em 1em",
+               border: "none",
+               boxShadow: "none"
+            }} 
+             size="large"
+             placeholder="Find People and Clubs on Clubhouse Chat..."
+             prefix={<img src="./images/search.png" alt="" width="15px" />}
+            ></Input>
+            </div>
             <h5>PEOPLE TO FOLLOW </h5>
             <div className={style.peopleContainer}>
                 {people.map((item) => (
